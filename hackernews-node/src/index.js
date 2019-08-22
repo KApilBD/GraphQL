99bd9ba164d14f32f2ff76f36cdd903df8d2ac3e
KApilBD <kapilbaraskar91@gmail.com>
2019-08-22 22:36:25 +0530

    fixed the path issue

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -5,39 +5,39 @@ const { GraphQLServer } = require('graphql-yoga');
 
 //Dummy Data
 let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }]
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 
-  let idCount = links.length
-  const resolvers = {
-    Query: {
-      info: () => `This is the API of a Hackernews Clone`,
-      feed: () => links,
-    },
-    Mutation: {
-      // 2
-      post: (parent, args) => {
-         const link = {
-          id: `link-${idCount++}`,
-          description: args.description,
-          url: args.url,
-        }
-        links.push(link)
-        return link
+let idCount = links.length
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
       }
-    },
-    // Link:{
-    //     id : (parent) => parent.id,
-    //     description :  (parent) => parent.description,
-    //     url : (parent) => parent.url,
-    // }
+      links.push(link)
+      return link
+    }
+  },
+  // Link:{
+  //     id : (parent) => parent.id,
+  //     description :  (parent) => parent.description,
+  //     url : (parent) => parent.url,
+  // }
 }
 
 const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers,
-  })
+  typeDefs: 'src/schema.graphql',
+  resolvers,
+})
 
 server.start(()=> console.log(`Server is running on http://localhost:4000`));
