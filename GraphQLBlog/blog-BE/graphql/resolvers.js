1bf1a92d1c0e715678d4f969c57c704ba7cd1483
KApilBD <kapilbaraskar91@gmail.com>
2020-04-03 23:04:40 +0530

    validation

--- GraphQLBlog/blog-BE/graphql/resolvers.js
+++ GraphQLBlog/blog-BE/graphql/resolvers.js
@@ -1,11 +1,27 @@
 const User = require("../models/user");
 const bcrypt = require('bcryptjs');
+const validator = require("validator");
 
 
 module.exports = {
     createUser: async function ({ userInput }, req) {
         // const email = args.userInput.email;
+        const errors = []
         const email = userInput.email;
+        if (!validator.isEmail(email)) {
+            errors.push({ message: "Email is invalid!" });
+        }
+        if (
+            validator.isEmpty(userInput.password) ||
+            validator.isLength(userInput.password, { min: 5 })
+        ) {
+            errors.push({ message: "Password is invalid!" });
+        }
+
+        if (errors.length > 0) {
+            const error = new Error("Invalid input.")
+            throw error;
+        }
         const existingUser = await User.findOne({ email: email });
         if (existingUser) {
             const error = new Error(" User Exist already !");
