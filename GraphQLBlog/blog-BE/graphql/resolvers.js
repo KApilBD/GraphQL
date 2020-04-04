567e56e6460440117e030eaba9e1d13d47f7c84f
KApilBD <kapilbaraskar91@gmail.com>
2020-04-04 13:24:52 +0530

    Error Handling

--- GraphQLBlog/blog-BE/graphql/resolvers.js
+++ GraphQLBlog/blog-BE/graphql/resolvers.js
@@ -19,7 +19,9 @@ module.exports = {
         }
 
         if (errors.length > 0) {
-            const error = new Error("Invalid input.")
+            const error = new Error("Invalid input.");
+            error.data = errors;
+            error.code = 422;
             throw error;
         }
         const existingUser = await User.findOne({ email: email });
