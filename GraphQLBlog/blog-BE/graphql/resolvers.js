12ee34ef20e5f3235f37ef032f356fd3bdcbffee
KApilBD <kapilbaraskar91@gmail.com>
2020-04-05 15:22:59 +0530

    signup using GQL

--- GraphQLBlog/blog-BE/graphql/resolvers.js
+++ GraphQLBlog/blog-BE/graphql/resolvers.js
@@ -8,23 +8,30 @@ module.exports = {
         // const email = args.userInput.email;
         const errors = []
         const email = userInput.email;
+        console.log("email",email, userInput)
         if (!validator.isEmail(email)) {
             errors.push({ message: "Email is invalid!" });
         }
+       
         if (
             validator.isEmpty(userInput.password) ||
-            validator.isLength(userInput.password, { min: 5 })
+            !validator.isLength(userInput.password, { min: 5 })
         ) {
+            console.log("ABD1")
             errors.push({ message: "Password is invalid!" });
         }
-
+      
         if (errors.length > 0) {
+            console.log("ABD2")
             const error = new Error("Invalid input.");
             error.data = errors;
             error.code = 422;
+            console.log("ABD2")
             throw error;
         }
+        console.log("ABD3")
         const existingUser = await User.findOne({ email: email });
+        console.log("ABD",existingUser)
         if (existingUser) {
             const error = new Error(" User Exist already !");
             throw error
